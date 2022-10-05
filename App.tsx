98092522b068bd42cf2959b0584149081fecbc80
HyeonWooGa <yeonwoopark22@gmail.com>
2022-10-06 02:41:08 +0900

    ⚡️ 헤더 상태 통합

--- App.tsx
+++ App.tsx
@@ -1,27 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
-import { theme } from "./color";
 
 export default function App() {
-  const [todoIng, setTodoIng] = useState(true);
-  const [targetIng, setTargetIng] = useState(false);
-  const [dreamIng, setDreamIng] = useState(false);
+  const [category, setCategory] = useState({
+    todo: true,
+    target: false,
+    dream: false,
+  });
 
   const todo = () => {
-    setTodoIng(true);
-    setTargetIng(false);
-    setDreamIng(false);
+    setCategory({ todo: true, target: false, dream: false });
   };
   const target = () => {
-    setTodoIng(false);
-    setTargetIng(true);
-    setDreamIng(false);
+    setCategory({ todo: false, target: true, dream: false });
   };
   const dream = () => {
-    setTodoIng(false);
-    setTargetIng(false);
-    setDreamIng(true);
+    setCategory({ todo: false, target: false, dream: true });
   };
 
   return (
@@ -32,7 +27,7 @@ export default function App() {
           <Text
             style={{
               ...styles.btnText,
-              color: todoIng ? theme.white : theme.gray,
+              color: category.todo ? "white" : "gray",
             }}
           >
             Todo
@@ -42,7 +37,7 @@ export default function App() {
           <Text
             style={{
               ...styles.btnText,
-              color: targetIng ? theme.white : theme.gray,
+              color: category.target ? "white" : "gray",
             }}
           >
             Target
@@ -52,7 +47,7 @@ export default function App() {
           <Text
             style={{
               ...styles.btnText,
-              color: dreamIng ? theme.white : theme.gray,
+              color: category.dream ? "white" : "gray",
             }}
           >
             Dream
@@ -66,7 +61,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: theme.bg,
+    backgroundColor: "black",
     paddingHorizontal: 20,
   },
   header: {
